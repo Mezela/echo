f5948edefa22502afe5b77bf199e9e9ba96d2999
Melvin Lau <lau.melvin@icloud.com>
2019-09-25 17:39:35 +0100

    Updated

--- lib/echo.rb
+++ lib/echo.rb
@@ -1,3 +1,4 @@
+require "time"
 class Echo
 
   def prompt
@@ -7,5 +8,13 @@ class Echo
   def get_input
     input = gets.chomp
     # puts "You said: #{input}"
+  end
+
+  def get_time
+    Time.now.strftime("%d/%m/%Y %H:%M")
+  end
+
+  def print
+    "#{get_time} | You said: '#{get_input}'!"
   end
 end
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -12,4 +12,19 @@ describe '#get_input' do
     expect(subject.get_input).not_to be nil
   end
 end
+
+describe "#get_time" do
+  it "gets the current time" do
+    expect(subject.get_time).to eq Time.now.strftime("%d/%m/%Y %H:%M")
+  end
+end
+
+describe "#print" do
+  it "prints the correct output" do
+
+    expect(subject.print).to eq "#{subject.get_time} | You said: '#{subject.get_input}'!"
+  end
+end
+
+
 end
